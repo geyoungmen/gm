912287f14ade055e95aa1c87e964ed7cfabe5850
leemp <leemp@DESKTOP-JM4E57T>
2024-01-26 15:09:20 +0900

    수정 및 kakao api을 이용한 팝업창으로 kakao map 띄우기

--- Mydemo/src/main/java/com/example/demo/controller/BoardController.java
+++ Mydemo/src/main/java/com/example/demo/controller/BoardController.java
@@ -6,22 +6,18 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.dto.BoardForm;
 import com.example.demo.service.BoardService;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class BoardController {
 	@Autowired
 	BoardService boardservice;
 	
-	@RequestMapping(value = "/board", method=RequestMethod.GET)
+	@GetMapping("/board")
 	public String insertBoard(HttpServletRequest request, Model mv) {
 		
 		List<BoardForm> boardList = boardservice.getBoard();
@@ -31,14 +27,15 @@ public class BoardController {
 		return "content/board";
 	}
 	
-	@RequestMapping(value = "/writeBoard", method=RequestMethod.GET)
+	@GetMapping("/writeBoard")
 	public String setWriteBoard() {
 		
 		return "content/writeBoard";
 	}
 	
 	//ajax를 활용해서 data를 개별로 받아오기
-	@RequestMapping(value = "/writeBoard", method=RequestMethod.POST)
+	//@RequestMapping(value = "/writeBoard", method=RequestMethod.POST)
+	@PostMapping("/writeBoard")
 	@ResponseBody
 	public int addWriteBoard(@RequestParam("brdSub") String brdSub, @RequestParam("brdContent") String brdContent){
 		BoardForm boardForm = new BoardForm();
@@ -50,19 +47,23 @@ public class BoardController {
 		return k;
 	}
 	
-	@RequestMapping(value = "/detailBoard", method=RequestMethod.GET)
+	//@RequestMapping(value = "/detailBoard", method=RequestMethod.GET)
+	@GetMapping("/detailBoard")
 	public String detailBoard(@RequestParam Map<String, String> Params, Model mv) {
-		System.out.println("Params : " + Params);
+		//System.out.println("Params : " + Params);
+		
 		BoardForm boardForm = new BoardForm();
 		boardForm.setBrdNo(Integer.parseInt(Params.get("No")));
 		
 		Map<String, Object> detailBoard = boardservice.detailBoard(boardForm);
+		
 		mv.addAttribute("detailboard", detailBoard);
 		
 		return "content/detailBoard";
 	}
 	
-	@RequestMapping(value = "/deleteBoard", method=RequestMethod.POST)
+	//@RequestMapping(value = "/deleteBoard", method=RequestMethod.POST)
+	@PostMapping("/deleteBoard")
 	@ResponseBody
 	public String deleteBoard(@RequestBody Map<String, String> data) {
 		
@@ -82,4 +83,44 @@ public class BoardController {
 	    return "content/board";
 	}
 	
+	
+	  @GetMapping("/updateBoard") 
+	  public String updatesetBoard(@RequestParam Map<String, String> Params, Model mv) {
+		BoardForm boardForm = new BoardForm();
+		boardForm.setBrdNo(Integer.parseInt(Params.get("No")));
+	  
+		Map<String, Object> detailBoard = boardservice.detailBoard(boardForm);
+	  
+		mv.addAttribute("detailboard", detailBoard);
+	  
+	  return "content/updateBoard"; 
+	  }
+	  
+	  @PostMapping("/updateBoard")
+	  @ResponseBody
+	  public String updateBoard(@RequestBody Map<String, String> data) {
+			
+	    String brdSub = data.get("brdSub");
+	    String brdContent = data.get("brdContent");
+	    String brdNoStr = data.get("brdNo");
+	    
+	    int brdNo = Integer.parseInt(brdNoStr);
+		    
+	    BoardForm boardForm = new BoardForm();
+	    boardForm.setBrdSub(brdSub);
+	    boardForm.setBrdContent(brdContent);
+	    boardForm.setBrdNo(brdNo);
+		    
+	    boardservice.updateBoard(boardForm);
+		    
+	    return "content/board";
+		}
+	  
+	  //카카오맵 api 연동 및 팝업창 띄우기
+	  @GetMapping("/kakaoMap")
+	  public String kakaoMap() {
+		  
+		  return "content/kakaoMap";
+	  }
+	  	 
 }
